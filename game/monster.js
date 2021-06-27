39a596a6f96f3cc994faa2e6a7fcc0b5851cc554
Bruno Pagno <brunopagno@gmail.com>
2021-06-27 12:55:13 +0200

    improve monster's AI

--- game/monster.js
+++ game/monster.js
@@ -1,19 +1,67 @@
 class Monster extends Character {
-  async act(target) {
-    let goX = this.x - target.x;
-    let goY = this.y - target.y;
+  async act(hero) {
+    let goX = this.x - hero.x;
+    let goY = this.y - hero.y;
     if (Math.abs(goX) > Math.abs(goY)) {
       if (goX > 0) {
-        await this.moveLeft();
+        const target = this._board.getXY(this.x - 1, this.y);
+        if (this.canAimAtTarget(target)) {
+          await this.moveLeft();
+        } else {
+          if (goY > 0) {
+            await this.moveUp();
+          } else if (goY < 0) {
+            await this.moveDown();
+          } else {
+            await this.moveLeft();
+          }
+        }
       } else {
-        await this.moveRight();
+        const target = this._board.getXY(this.x + 1, this.y);
+        if (this.canAimAtTarget(target)) {
+          await this.moveRight();
+        } else {
+          if (goY > 0) {
+            await this.moveUp();
+          } else if (goY < 0) {
+            await this.moveDown();
+          } else {
+            await this.moveRight();
+          }
+        }
       }
     } else {
       if (goY > 0) {
-        await this.moveUp();
+        const target = this._board.getXY(this.x, this.y - 1);
+        if (this.canAimAtTarget(target)) {
+          await this.moveUp();
+        } else {
+          if (goX > 0) {
+            await this.moveLeft();
+          } else if (goX < 0) {
+            await this.moveRight();
+          } else {
+            await this.moveUp();
+          }
+        }
       } else {
-        await this.moveDown();
+        const target = this._board.getXY(this.x, this.y + 1);
+        if (this.canAimAtTarget(target)) {
+          await this.moveDown();
+        } else {
+          if (goX > 0) {
+            await this.moveLeft();
+          } else if (goX < 0) {
+            await this.moveRight();
+          } else {
+            await this.moveDown();
+          }
+        }
       }
     }
+  }
+
+  canAimAtTarget(target) {
+    return target instanceof EmptySpace || target instanceof Character;
   }
 }
